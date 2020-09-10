3e5b37e276d19d719beb3d985e9c980282d99aa5
Marco Plá <marco.pla@gmail.com>
2020-09-09 21:26:03 -0300

    Number and Math finished

--- js/main.js
+++ js/main.js
@@ -14,21 +14,35 @@ console.log(Math.round(Math.random()* (2000 - 1050 + 1) + 1050));
 let min = 9999;
 const numeros = '4, 5, 20, 8, 9';
 let numerosArray = numeros.split(', ');
+console.log(Math.min(...numerosArray));
 
-numerosArray.forEach((numero,index)=> {
+/*numerosArray.forEach((numero,index)=> {
   numerosArray[index] = Number.parseInt(numero);
-  if(numerosArray[index] < min){
+  if(numerosArray[index] < min){  
     min = numerosArray[index];
   }
 });
 console.log(`O menor número é: ${min}`);
-
+*/
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
 const listaPrecos = ['R$ 59,99', ' R$ 100,222','R$ 230  ', 'r$  200'];
 
+function limpaPreco(preco){
+  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+  preco = +preco.toFixed(2);
+  return preco;
+}
+
+let somaPrecos = 0;
+listaPrecos.forEach((preco) => {
+  somaPrecos += limpaPreco(preco);
+})
+console.log(somaPrecos.toLocaleString('pt-BR', {style:'currency', currency:'BRL'}));
+limpaPreco(listaPrecos[0]);
+/*
 function limpaPrecos(listaPrecos){
   let somaPrecos = 0;
   let arrayPrecos = [];
@@ -40,3 +54,5 @@ function limpaPrecos(listaPrecos){
   console.log(arrayPrecos);
 
 }   
+
+*/
