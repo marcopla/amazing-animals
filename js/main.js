7e34e1895d769c4eeb2c63bcbfeaf84fa3fd7eae
Marco Plá <marco.pla@gmail.com>
2020-09-10 21:48:40 -0300

    array methods

--- js/main.js
+++ js/main.js
@@ -1,58 +1,24 @@
-let valor = 48.49;
-console.log(valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const primeiraComida = comidas.shift();
+// Remova o último valor de comidas e coloque em uma variável
+// Adicione 'Arroz' ao final da array
+// Adicione 'Peixe' e 'Batata' ao início da array
 
-console.log(valor.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+// Inverta a ordem dos estudantes
+// Verifique se Joana faz parte dos estudantes
+// Verifique se Juliana faz parte dos estudantes
 
-////////Exercícios
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`
+// Substitua section por ul e div com li,
+// utilizando split e join
 
-// Retorne um número aleatório
-// entre 1050 e 2000
-
-console.log(Math.round(Math.random()* (2000 - 1050 + 1) + 1050));
-
-// Retorne o maior número da lista abaixo
-let min = 9999;
-const numeros = '4, 5, 20, 8, 9';
-let numerosArray = numeros.split(', ');
-console.log(Math.min(...numerosArray));
-
-/*numerosArray.forEach((numero,index)=> {
-  numerosArray[index] = Number.parseInt(numero);
-  if(numerosArray[index] < min){  
-    min = numerosArray[index];
-  }
-});
-console.log(`O menor número é: ${min}`);
-*/
-
-// Crie uma função para limpar os preços
-// e retornar os números com centavos arredondados
-// depois retorne a soma total
-const listaPrecos = ['R$ 59,99', ' R$ 100,222','R$ 230  ', 'r$  200'];
-
-function limpaPreco(preco){
-  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
-  preco = +preco.toFixed(2);
-  return preco;
-}
-
-let somaPrecos = 0;
-listaPrecos.forEach((preco) => {
-  somaPrecos += limpaPreco(preco);
-})
-console.log(somaPrecos.toLocaleString('pt-BR', {style:'currency', currency:'BRL'}));
-limpaPreco(listaPrecos[0]);
-/*
-function limpaPrecos(listaPrecos){
-  let somaPrecos = 0;
-  let arrayPrecos = [];
-  listaPrecos.forEach((preco) => {
-    console.log(Number.parseFloat(preco).toFixed(2));
-    arrayPrecos.push(Number.parseFloat(preco).toFixed(2));
-    somaPrecos += Number.parseFloat(preco).toFixed(2);
-  }); 
-  console.log(arrayPrecos);
-
-}   
-
-*/
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
