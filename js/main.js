7eb7732cf2ca15dd7a1fb3f72723bf1befe1f8ce
Marco Plá <marco.pla@gmail.com>
2020-09-10 23:33:10 -0300

    Array fineshed

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@ const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
 // Remova o primeiro valor de comidas e coloque em uma variável
 const primeiraComida = comidas.shift();
 // Remova o último valor de comidas e coloque em uma variável
+const ultimaComida = comidas.pop();
 // Adicione 'Arroz' ao final da array
+comidas.push('Arroz');
 // Adicione 'Peixe' e 'Batata' ao início da array
+comidas.unshift('Peixe','Batata');
 
 const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Arrume os estudantes em ordem alfabética
+estudantes.sort();
 // Inverta a ordem dos estudantes
+estudantes.reverse();
 // Verifique se Joana faz parte dos estudantes
+estudantes.includes('Joana');
 // Verifique se Juliana faz parte dos estudantes
+estudantes.includes('Juliana');
 
 let html = `<section>
               <div>Sobre</div>
@@ -19,6 +26,12 @@ let html = `<section>
 // Substitua section por ul e div com li,
 // utilizando split e join
 
+html = html.split('<section>').join('<ul>').split('</section>').join('</ul>').;
+html = html.split('<div>').join('<li>').split('</div>').join('</li>');
+
+
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
+let carroArray = carros.slice();
+carroArray.pop();
