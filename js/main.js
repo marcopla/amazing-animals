0af62949c5e9c2198b9a8922b18c643a18d0f1e7
Marco Plá <marco.pla@gmail.com>
2020-09-11 23:41:55 -0300

    array-iteration

--- js/main.js
+++ js/main.js
@@ -1,37 +1,4 @@
-const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
-// Remova o primeiro valor de comidas e coloque em uma variável
-const primeiraComida = comidas.shift();
-// Remova o último valor de comidas e coloque em uma variável
-const ultimaComida = comidas.pop();
-// Adicione 'Arroz' ao final da array
-comidas.push('Arroz');
-// Adicione 'Peixe' e 'Batata' ao início da array
-comidas.unshift('Peixe','Batata');
+const valores = [10, 11, 33, 44, 55];
+const valorX2 = valores.map((item) => item * 2);
 
-const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
-// Arrume os estudantes em ordem alfabética
-estudantes.sort();
-// Inverta a ordem dos estudantes
-estudantes.reverse();
-// Verifique se Joana faz parte dos estudantes
-estudantes.includes('Joana');
-// Verifique se Juliana faz parte dos estudantes
-estudantes.includes('Juliana');
-
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`
-// Substitua section por ul e div com li,
-// utilizando split e join
-
-html = html.split('<section>').join('<ul>').split('</section>').join('</ul>').;
-html = html.split('<div>').join('<li>').split('</div>').join('</li>');
-
-
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-// Remova o último carro, mas antes de remover
-// salve a array original em outra variável
-let carroArray = carros.slice();
-carroArray.pop();
+console.log(valorX2);
