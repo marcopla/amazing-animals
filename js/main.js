5e84509533ea45f91abf1c4f26671891d40ef22e
Marco Plá <marco.pla@gmail.com>
2020-09-12 22:30:52 -0300

    .map()

--- js/main.js
+++ js/main.js
@@ -2,3 +2,24 @@ const valores = [10, 11, 33, 44, 55];
 const valorX2 = valores.map((item) => item * 2);
 
 console.log(valorX2);
+
+const aulas = [
+  {
+    nome: 'HTML 1',
+    min: 15
+  },
+  {
+    nome: 'HTML 2',
+    min: 10
+  },
+  {
+    nome: 'CSS 1',
+    min: 20
+  },
+  {
+    nome: 'JS 1',
+    min: 25
+  },
+];
+
+const arrayMin = aulas.map((aula) => aula.min);
