300319ec131539124e8213f4472301fc88cfdd13
Marco Plá <marco.pla@gmail.com>
2020-09-23 23:15:00 -0300

    function

--- js/main.js
+++ js/main.js
@@ -1,137 +1,14 @@
-const valores = [10, 11, 33, 44, 55];
-const valorX2 = valores.map((item) => item * 2);
+const perimetro = new Function('lado', 'return lado * 4');
 
-console.log(valorX2);
+function Dom(seletor){
+  this.element = document.querySelector(seletor);
+}
 
-const aulas = [
-  {
-    nome: 'HTML 1',
-    min: 15
-  },
-  {
-    nome: 'HTML 2',
-    min: 10
-  },
-  {
-    nome: 'CSS 1',
-    min: 20
-  },
-  {
-    nome: 'JS 1',
-    min: 25
-  },
-];
+Dom.prototype.ativo = function(classe){
+  this.element.classList.add(classe);
+}
 
-const arrayMin = aulas.map((aula) => aula.min);
-/*
-const numeros = [10, 25, 60, 5, 35, 10];
+const ul = new Dom('ul');
 
-const maiorValor = numeros.reduce((anterior, atual) => {
-  return anterior < atual ? atual : anterior;
-});
-
-maiorValor; // 60
-*/
-const frutas = ['Banana', 'Pêra', 'Uva'];
-
-const frutasRight = frutas.reduceRight((acc, fruta) => acc + ' ' + fruta);
-const frutasLeft = frutas.reduce((acc, fruta) => acc + ' ' + fruta);
-
-frutasRight; // Uva Pêra Banana
-frutasLeft; // Banana Pêra Uva
-
-const listaAulas = aulas.reduce((acumulador, atual, index) => {
-  acumulador[index] = atual.nome;
-  return acumulador;
-}, {});
-
-const temUva = frutas.some((fruta) => {
-  return fruta === 'Uva';
-}); 
-//true
-
-const every = frutas.every((item) => {
-  return item === 'Uva';
-});
-// false pois nem todos são Uva
-
-const buscaUva = frutas.findIndex((fruta) => {
-  return fruta === 'Uva';
-}); // 2
-
-
-////////// Exercicio
-
-
-
-// Selecione cada curso e retorne uma array
-// com objetos contendo o título, descricao,
-// aulas e horas de cada curso
-
-const cursosQuery = document.querySelectorAll('.curso');
-const cursos = Array.from(cursosQuery);
-
-const objetoCursos = cursos.map(curso => {
-  
-  const titulo = curso.querySelector('h1').innerText;
-  const descricao = curso.querySelector('p').innerText;
-  const aulas = curso.querySelector('.aulas').innerText;
-  const horas = curso.querySelector('.horas').innerText;
-  
-  return {
-    titulo,
-    descricao,
-    aulas,
-    horas 
-  }
-
-});
-
-// Retorne uma lista com os
-// números maiores que 100
-const numeros = [3, 44, 333, 23, 122, 322, 33];
-
-const maioresQ100 = numeros.filter( numero => {
-    return numero > 100;
-  });
-
-
-// Verifique se Baixo faz parte
-// da lista de instrumentos e retorne true
-const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
-
-const containBaixo = instrumentos.some( instrumento => {
-  return instrumento == 'Baixo'
-  }
-);
-
-// Retorne o valor total das compras
-const compras = [
-  {
-    item: 'Banana',
-    preco: 'R$ 4,99'
-  },
-  {
-    item: 'Ovo',
-    preco: 'R$ 2,99'
-  },
-  {
-    item: 'Carne',
-    preco: 'R$ 25,49'
-  },
-  {
-    item: 'Refrigerante',
-    preco: 'R$ 5,35'
-  },
-  {
-    item: 'Quejo',
-    preco: 'R$ 10,60'
-  }
-]
-
-const precoTotal = compras.reduce( (acumulador, compra) => {
-  const precoString = compra.preco.slice(3).replace(',','.');
-  const preco = +precoString;
-  console.log(preco);
-  return acumulador + preco;
-},0);
+console.log(ul);
+console.log(ul.element);
