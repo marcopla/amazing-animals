e53151b0062fc8ec03c46940de9a0348e13f308d
Marco Plá <marco.pla@gmail.com>
2020-09-25 23:29:52 -0300

    function.call() exemple

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
-const perimetro = new Function('lado', 'return lado * 4');
+// const perimetro = new Function('lado', 'return lado * 4');
 
-function Dom(seletor){
-  this.element = document.querySelector(seletor);
-}
+// function Dom(seletor){
+//   this.element = document.querySelector(seletor);
+// }
 
-Dom.prototype.ativo = function(classe){
+// Dom.prototype.ativo = function(classe){
+//   this.element.classList.add(classe);
+// }
+
+// const ul = new Dom('ul');
+
+// const li = {
+//   element: document.querySelector('li');
+// }
+// // console.log(ul);
+// // console.log(ul.element);
+// ul.ativo.call(li, 'ativo');
+
+
+const frutas = ['Uva', 'Maçã', 'Banana'];
+
+Array.prototype.mostrarThis = function() {
   console.log(this);
-  this.element.classList.add(classe);
 }
 
-const ul = new Dom('ul');
+Array.prototype.pop.call(frutas);
+frutas.pop();
 
-const li = {
-  element: document.querySelector('li');
+const arrayLike = {
+  0: 'Item 1',
+  1: 'Item 2',
+  2: 'Item 3',
+  length: 3
 }
-// console.log(ul);
-// console.log(ul.element);
-ul.ativo.call(li, 'ativo');
