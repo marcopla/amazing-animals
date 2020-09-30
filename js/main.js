fb32a03293d97ea15a5c11fa9b41402399b2a0e1
Marco Plá <marco.pla@gmail.com>
2020-09-29 23:48:04 -0300

    function exercice

--- js/main.js
+++ js/main.js
@@ -67,3 +67,15 @@ const aceleraHonda = carro.acelerar.bind(honda);
 
 console.log(aceleraHonda(300, 20));
 
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
