b2b9bc1913733fac91f99fdc7612eed524f9eb87
Marco Plá <marco.pla@gmail.com>
2020-10-01 23:54:44 -0300

    exercice 1

--- js/main.js
+++ js/main.js
@@ -69,13 +69,26 @@ console.log(aceleraHonda(300, 20));
 
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('p');
 
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, item) => {
+  return acumulador + item.innerText.length;
+}, 0);
+
+console.log(totalCaracteres);
+console.log(paragrafos);
 
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
+function criarElemento (tag, classe, conteudo) {
+  const elemento  = document.createElement(tag);
+  classe ? elemento.classList.add(classe) : null;
+  conteudo ? elemento.innerHtml = conteudo : null;
+  return elemento;
+}
 
-
+console.log(criarElemento('li', 'ativa'));
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
