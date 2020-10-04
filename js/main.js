263980dc17073fed7a844f1b997690811439fa08
Marco Plá <marco.pla@gmail.com>
2020-10-03 23:50:46 -0300

    Object.create() and Object.assign()

--- js/main.js
+++ js/main.js
@@ -6,12 +6,46 @@ const pessoa = new Object({
 console.log(pessoa);
 
 const carro = {
-  marca: 'Fiat',
   rodas: 4,
+  init(marca){
+    this.marca = marca;
+    return this;
+  },
   acelerar(){
-    return this.marca + acelerou;
+    return this.marca + ' acelerou';
   },
   buzinar(){
-    return this.marca + 'Buzinou';
+    return this.marca + ' buzinou';
   }
 }
+
+const ferrari = Object.create(carro).init('Ferrari');
+
+//honda.marca = 'Honda';
+//honda.init('Honda')
+
+ferrari.acelerar;
+
+const funcaoAutomovel = {
+  acelerar() {
+    return 'acelerou';
+  },
+  buzinar() {
+    return 'buzinou';
+  },
+
+}
+
+const moto = {
+  rodas: 2,
+  capacete: true,
+};
+
+const caminhao = {
+  rodas: 4, 
+  mala: true,
+};
+
+Object.assign(moto, funcaoAutomovel);
+Object.assign(caminhao, funcaoAutomovel);
+
