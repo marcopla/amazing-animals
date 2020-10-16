8420b158a56958c4d041e1b820cbb47132907388
Marco Plá <marco.pla@gmail.com>
2020-10-15 23:20:12 -0300

    dataset propertys

--- animacoes.js
+++ animacoes.js
@@ -1,4 +1,3 @@
-const h1 = document.querySelector( 'h1');
+const div = document.querySelector('div');
 
-console.log(Object.prototype.toString.call(h1));
-console.log(h1.dataset);
+console.log(div.dataset.cor);
