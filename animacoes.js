8e82f13dfe4c90a27734d8134dae86550a4b27ca
Marco Plá <marco.pla@gmail.com>
2020-10-16 23:21:03 -0300

    delete property

--- animacoes.js
+++ animacoes.js
@@ -1,3 +1,5 @@
-const div = document.querySelector('div');
-
+const div = document.querySelector('[data-cor]');
+div.dataset.height = 1000;
 console.log(div.dataset.cor);
+
+delete div.dataset.width;
