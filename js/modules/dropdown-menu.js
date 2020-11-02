c8a55f98ff3b9293a5b52d52f51a71646a73b35a
Marco Plá <marco.pla@gmail.com>
2020-11-02 00:40:40 -0300

    dropdown-menu completed

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,15 @@
 export default function initDropdownMenu (){
 
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+  dropdownMenus.forEach( menu => {
+    ['touchstart', 'click'].forEach( userEvent => {
+      menu.addEventListener( userEvent, handleclick);
+    });
+  });
+
+  function handleclick(event){
+    event.preventDefault();
+    this.classList.toggle('active');
+  };
 }
