59ea9a958070dac2c550b394f06f58e9c56e1bed
Marco Plá <marco.pla@gmail.com>
2020-11-11 21:32:44 -0300

    executa

--- settimeout.js
+++ settimeout.js
@@ -7,3 +7,9 @@ setTimeout(espera, 1000, 'espera');
 setTimeout(() =>{
   console.log('Após 0s?');
 });
+
+function executa (texto){
+  console.log(texto);
+}
+
+setTimeout(espera2, 1000, 'executa');
