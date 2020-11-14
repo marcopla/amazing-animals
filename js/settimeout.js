a30dbd242a94cd8d15ab6559de1b5af587aba2cd
Marco Plá <marco.pla@gmail.com>
2020-11-14 20:19:23 -0300

    setInterval and clearInterval

--- js/settimeout.js
+++ js/settimeout.js
@@ -1,3 +1,31 @@
+const btn = document.querySelector('button');
+btn.addEventListener('click', handleClick);
+
+function handleClick(){
+  setTimeout(function(){
+    this.classList.add('active');
+  });
+}
+
+
+let contarAte10 = setInterval(callback, 1000);
+
+let = 0;
+
+function callback(){
+  console.log(i++);
+  if(i > 10){
+    clearInterval(contarAte10);
+  }
+}
+
+
+/*
+function handleClick(){
+  console.log(this);
+  this.classList.add('active');
+}
+
 function espera (texto){
   console.log(texto);
 }
@@ -13,4 +41,5 @@ for(let i = 0; i < 20 ; i++){
     console.log(i);
   }, 1000);
 }
+*/
 
