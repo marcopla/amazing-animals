b0fc2322d0f969066050e3753f51a8ceb3d1e6cd
Marco Plá <marco.pla@gmail.com>
2020-11-19 16:32:51 -0300

    exercice 2 finished

--- js/settimeout.js
+++ js/settimeout.js
@@ -1,35 +1,66 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', handleClick);
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', handleClick);
 
-function handleClick(){
-  setTimeout(function(){
-    this.classList.add('active');
-  });
-}
-
-
-let contarAte10 = setInterval(callback, 1000);
-
-let = 0;
-
-function callback(){
-  console.log(i++);
-  if(i > 10){
-    clearInterval(contarAte10);
-  }
-}
+// function handleClick(){
+//   setTimeout(function(){
+//     this.classList.add('active');
+//   });
+// }
 
 
-//Exercícios
+// let contarAte10 = setInterval(callback, 1000);
+
+// let i = 0;
+
+// function callback(){
+//   console.log(i++);
+//   if(i > 10){
+//     clearInterval(contarAte10);
+//   }
+// }
+
+
+////////////////////////////////////////Exercícios
+
+/*
+Exercício 1
 function mudarClasse(){
   document.body.classList.toggle('active');
 }
 
 setInterval(mudarClasse, 2000);
+*/
 
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const tempo = document.querySelector('.tempo');
+
+iniciar.addEventListener('click', iniciarTempo);
+pausar.addEventListener('click', pausarTempo);
+pausar.addEventListener('dblclick', resetarTempo);
+
+let i = 0;
+let timer;
+
+function iniciarTempo(){
+  timer = setInterval(() => {
+    tempo.innerText = i++;
+  }, 1000);
+  iniciar.setAttribute('disabled', '');
+}
+
+function pausarTempo(){
+  clearInterval(timer);
+  iniciar.removeAttribute('disabled');
+  
+}
+function resetarTempo(){
+  i = 0;
+  clearInterval(timer);
+  tempo.innerText = 0;
+}
+
+
 /*
 function handleClick(){
   console.log(this);
