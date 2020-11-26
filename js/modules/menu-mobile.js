e2a2c8b725dd257c98d16ecf5f3d55049ca7ecaf
Marco Plá <marco.pla@gmail.com>
2020-11-26 10:04:24 -0300

    menu-mobile bug fixed

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -2,17 +2,22 @@ import outsideClick from './outsideclick.js'
 
 export default function initMenuMobile() {
 
-
-
 const menuButton = document.querySelector('[data-menu="button"]');
 const menuList = document.querySelector('[data-menu="list"]');
+const eventos = ['click', 'touchstart'];
 
  function openMenu(event){
   menuButton.classList.add('active');
   menuList.classList.add('active');
+  outsideClick(menuList, eventos, () => {
+    menuButton.classList.remove('active');
+    menuList.classList.remove('active');
+  });
  }
  
-
- menuButton.addEventListener('click', openMenu);
+eventos.forEach( evento =>{ 
+  menuButton.addEventListener(evento, openMenu);
+});
+ 
 
 }
