af9c6dea73b03179eb0378da476178062bd3e08a
Marco Plá <marco.pla@gmail.com>
2020-11-27 20:15:06 -0300

    menu-mobile finished

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -2,22 +2,18 @@ import outsideClick from './outsideclick.js'
 
 export default function initMenuMobile() {
 
-const menuButton = document.querySelector('[data-menu="button"]');
-const menuList = document.querySelector('[data-menu="list"]');
-const eventos = ['click', 'touchstart'];
-
- function openMenu(event){
-  menuButton.classList.add('active');
-  menuList.classList.add('active');
-  outsideClick(menuList, eventos, () => {
-    menuButton.classList.remove('active');
-    menuList.classList.remove('active');
-  });
- }
- 
-eventos.forEach( evento =>{ 
-  menuButton.addEventListener(evento, openMenu);
-});
- 
-
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click', 'touchstart'];
+  if(menuButton){
+    function openMenu(event){
+      menuButton.classList.add('active');
+      menuList.classList.add('active');
+      outsideClick(menuList, eventos, () => {
+        menuButton.classList.remove('active');
+        menuList.classList.remove('active');
+      });
+    }
+    eventos.forEach( evento => menuButton.addEventListener(evento, openMenu));
+  }
 }
