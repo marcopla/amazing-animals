9462b47807117f6f26f80f4d06d75e3805b5c4e1
Marco Plá <marco.pla@gmail.com>
2020-11-30 20:49:32 -0300

    anima-numeros timer added

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -3,4 +3,16 @@ export default function initAnimaNumeros(){
 }
 
 const numeros = document.querySelectorAll('[data-numero]');
-console.log(numeros);
+
+numeros.forEach( numero => {
+  const total = +numero.innerText;
+  const incremento = total / 100;
+  let start = 0;
+  const timer = setInterval(() => {
+    start = start + incremento;
+    numero.innerText = start;
+    if(start > total){
+      clearInterval(timer);
+    }
+  }, 0);
+});
