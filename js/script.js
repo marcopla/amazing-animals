4f27ae435deec831e1863fec4009fb68e3dc22c7
Marco Plá <marco.pla@gmail.com>
2020-12-29 22:51:43 -0300

    testando

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ initFuncionamento();
 const controles = document.getElementById('controles');
 const cssText = document.querySelector('.css');
 const btn = document.querySelector( '.btn');
-
+controles.addEventListener('change',handleChange);
 function handleChange(event){
   console.log(event);
 }
