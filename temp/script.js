87e90a85f7805be7ab1fd9f3a42467422710e0a0
Marco Plá <marco.pla@gmail.com>
2021-01-15 20:34:34 -0300

    .blob() function

--- temp/script.js
+++ temp/script.js
@@ -1,8 +1,10 @@
-/*
+/* arquivo
 const doc = fetch('./doc.txt'); // é uma promise
 
 doc
-.then(response => response.text())
+.then(response => {
+  return response.text()
+})
 .then((body) => {
   const conteudo = document.querySelector('.conteudo');
   conteudo.innerText = body;
@@ -11,11 +13,36 @@ doc
 
 */
 
+/* CEP
 const cep = fetch('https://viacep.com.br/ws/01001000/json/');
 
 cep
 .then(r => r.json())
+.then(body => {
+  const logradouro = body.logradouro;
+  console.log(logradouro);
+})
+*/
+
+/* HTML
+const sobre = fetch('./sobre.html');
+
+sobre
+.then(r => r.text())
 .then((body) => {
-  const conteudo = document.querySelector('.conteudo');
-  conteudo.innerText = body.logradouro;
+  const div = document.createElement('div');
+  div.innerHTML = body;
+  const titulo = div.querySelector('title');
+  document.querySelector('h1').innerHTML = titulo.innerHTML;
+  console.log(titulo);
 });
+
+*/
+
+const imagem = fetch('./image.png');
+
+imagem
+.then(r => r.blob())
+.then(body =>{
+  
+})
