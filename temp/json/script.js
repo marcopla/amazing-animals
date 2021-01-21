596390c44b66e4f3b17058834bb63e3e9b4376ae
Marco Plá <marco.pla@gmail.com>
2021-01-20 21:43:52 -0300

    .jsonfy() and .parse()

--- temp/json/script.js
+++ temp/json/script.js
@@ -8,3 +8,19 @@ cursos
   console.log(jsonFinal);
 });
 */
+cursos
+.then(r => r.json())
+.then(json => {
+
+})
+
+const configuracoes = {
+  player: "Google",
+  tempo: 25.5,
+  aula: "2.1 JavaScript"
+}
+
+localStorage.config = JSON.stringify(configuracoes);
+
+const stringConfig = JSON.parse(localStorage.config);
+console.log(stringConfig);
