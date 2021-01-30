b050e3146460b61f5b076cc9efd74c377939d8d0
Marco Plá <marco.pla@gmail.com>
2021-01-29 21:58:05 -0300

    class, static added

--- temp/script.js
+++ temp/script.js
@@ -1,15 +1,51 @@
-// function Button(text, background){
-//   this.text = text;
-//   this.background = background;
+function Button(text, background){
+  this.text = text;
+  this.background = background;
+}
+
+Button.prototype.element = function() {
+  const buttonElement = document.createElement('button');
+  buttonElement.innerText = this.text;
+  buttonElement.style.background = this.background; 
+  return buttonElement;
+}
+static 
+
+
+
+
+
+
+// class Button {
+//   constructor(text, background,color) {
+//     this.text = text;
+//     this.background = background;
+//     this.color = color;
+//   }
+//   element(){
+//     const buttonElement = document.createElement('button');
+//     buttonElement.innerText = this.text;
+//     buttonElement.style.background = this.background; 
+//     buttonElement.style.color = this.color;
+//     return buttonElement;
+//   }
+//   appendTo(target){
+//     const targetElement = document.querySelector(target);
+//     targetElement.appendChild(this.element());
+//     return targetElement;
+//   }
+//   static createButton(text, background) {
+//     const buttonElement = document.createElement('button');
+//     buttonElement.innerText = text;
+//     buttonElement.style.background = background;
+//     return buttonElement;
+//   }
 // }
 
-// Button.prototype.element = function() {
-//   const buttonElement = document.createElement('button');
-//   buttonElement.innerText = this.text;
-//   buttonElement.style.background = this.background; 
-//   return buttonElement;
-// }
+// const buttonBlue = new Button('Comprar', 'blue', 'white'); 
 
-const buttonBlue = new Button('Comprar', 'blue'); 
+// buttonBlue.appendTo('body');
 
-console.log(buttonBlue);
+// const redButtonStatic = Button.createButton('Click aqui', 'red');
+
+// console.log(redButtonStatic);
