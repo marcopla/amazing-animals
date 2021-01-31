87fe45650f11ffcc8f0a0811de6b50503adc7cb1
Marco Plá <marco.pla@gmail.com>
2021-01-31 12:16:10 -0300

    function-expression

--- countdown/script.js
+++
@@ -1,7 +0,0 @@
-import Countdown from './countdown.js';
-
-const diasParaNatal = new Countdown('24 December 2021 23:59:59 GMT-0300');
-
-setInterval(() => {
-  console.log(diasParaNatal.total);
-},1000);
