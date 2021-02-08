9a6528438d5d7f852594d96813ca9dacd3e45530
Marco Plá <marco.pla@gmail.com>
2021-02-07 23:14:52 -0300

    accordion transformed into class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
 import initAnimacaoScroll from './modules/animacao-scroll.js';
-import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import { testando as teste, testando2 } from './modules/teste.js';
 import * as tudo from './modules/teste2.js';
@@ -15,8 +15,10 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^= "#"');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initAnimacaoScroll();
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
