cc236ea9100b85e1b18a80946f5527c1f5c17f72
Marco Plá <marco.pla@gmail.com>
2021-02-14 18:46:25 -0300

    bugs fixed: font source added, arrows svgs added and responsive modal

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -1,6 +1,6 @@
 import debounce from './debounce.js';
 
-export class Slide {
+export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.querySelector(wrapper);
     this.slide = document.querySelector(slide);
@@ -137,60 +137,7 @@ export class Slide {
     this.addSlideEvents();
     this.slideConfig();
     this.addResizeEvent();
-    this.changeSlide(0);
+    this.changeSlide(2);
     return this;
-  }
-}
-
-export default class SlideNav extends Slide {
-  constructor(slide, wrapper) {
-    super(slide, wrapper);
-    this.bindControlEvents();
-  }
-
-  addArrow(prev, next) {
-    this.prevElement = document.querySelector(prev);
-    this.nextElement = document.querySelector(next);
-    this.addArrowEvent();
-  }
-
-  addArrowEvent() {
-    this.prevElement.addEventListener('click', this.activePrevSlide);
-    this.nextElement.addEventListener('click', this.activeNextSlide);
-  }
-
-  createControl() {
-    const control = document.createElement('ul');
-    control.dataset.control = 'slide';
-    this.slideArray.forEach((item, index) => {
-      control.innerHTML += `<li><a href=#slide${index + 1}">${index +1}</a></li>`;
-    });
-    this.wrapper.appendChild(control);
-    return control;
-  }
-
-  eventControl(item, index) {
-    item.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.changeSlide(index);
-    });
-    this.wrapper.addEventListener('changeEvent', this.activeControlItem);
-  }
-
-  activeControlItem() {
-    this.controlArray.forEach((item) => item.classList.remove(this.activeClass));
-    this.controlArray[this.index.active].classList.add(this.activeClass);
-  }
-
-  addControl(customControl) {
-    this.control = document.querySelector(customControl) || this.createControl();
-    this.controlArray = [...this.control.children];
-    this.activeControlItem();
-    this.controlArray.forEach(this.eventControl);
-  }
-
-  bindControlEvents() {
-    this.eventControl = this.eventControl.bind(this);
-    this.activeControlItem = this.activeControlItem.bind(this);
   }
 }
